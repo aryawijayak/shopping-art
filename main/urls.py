633bd9aac59b75f19723027496166b2c91fbc085
aryawijayak <aryawijayak2004@gmail.com>
2023-09-11 23:22:06 +0700

    Update Tutorial

--- main/urls.py
+++ main/urls.py
@@ -1,10 +1,8 @@
-
-from django.urls import path, include
-
+from django.urls import path
 from main.views import show_main
 
 app_name = 'main'
 
 urlpatterns = [
-    path('main/', include('main.urls')),
+    path('', show_main, name='show_main'),
 ]
