40014e2e16ece41e72a46446fde8e8f571dd5d6e
aryawijayak <aryawijayak2004@gmail.com>
2023-10-04 08:23:03 +0700

    Setting Vercel

--- shopping_art/wsgi.py
+++ shopping_art/wsgi.py
@@ -11,7 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'WaifuCollection.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'shopping_art.settings')
 
 application = get_wsgi_application()
 app = application
