d3383789d265967fa7a8018b242b5ff89c349c99
aryawijayak <aryawijayak2004@gmail.com>
2023-10-09 08:24:29 +0700

    Fix Error

--- shopping_art/settings.py
+++ shopping_art/settings.py
@@ -134,7 +134,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
