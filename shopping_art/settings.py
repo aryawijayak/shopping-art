399c3aadbf086b21b4d20e023e7412d79bb39e69
aryawijayak <aryawijayak2004@gmail.com>
2023-10-09 08:17:57 +0700

    Merapikan

--- shopping_art/settings.py
+++ shopping_art/settings.py
@@ -11,15 +11,15 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-import os
-from pathlib import Path
 import environ # Tambahkan kode berikut
+import os # Tambahkan kode berikut
 
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
 env = environ.Env()
 
 # Quick-start development settings - unsuitable for production
@@ -27,6 +27,9 @@ env = environ.Env()
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-u0xfuu7l$_l87&*qj87!a5@s(g77x3du)j=03*fz4%!cf+mpd3'
+
+# Automatically determine environment by detecting if DATABASE_URL variable.
+# DATABASE_URL is provided by Heroku if a database add-on is added (e.g. Heroku Postgres).
 PRODUCTION = env.bool('PRODUCTION', False)
 
 # SECURITY WARNING: don't run with debug turned on in production!
@@ -88,12 +91,12 @@ DATABASES = {
     }
 }
 
+# Set database settings automatically using DATABASE_URL.
 if PRODUCTION:
     DATABASES = {
         'default': env.db('DATABASE_URL')
     }
     DATABASES["default"]["ATOMIC_REQUESTS"] = True
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
